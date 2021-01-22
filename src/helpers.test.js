97857a7421d3102b5b2cc40828f1eed4b007b554
Saurabh Kumar <saurabh.k0402@gmail.com>
2021-01-22 13:56:20 +0530

    X number product test added

--- src/helpers.test.js
+++ src/helpers.test.js
@@ -17,3 +17,9 @@ test('Basic two number product', () => {
   expect(mul(42, 58)).toBe(42 * 58);
   expect(mul(56, 59)).toBe(56 * 59);
 });
+
+test('X Numbers product', () => {
+  expect(mul(1, 2)).toBe(2);
+  expect(mul(1, 2, 3)).toBe(1 * 2 * 3);
+  expect(mul(1, 2, 3, 4, 5, 6)).toBe(1 * 2 * 3 * 4 * 5 * 6);
+});
