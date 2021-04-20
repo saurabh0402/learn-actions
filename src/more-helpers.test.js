d898d0abfb1dbe6082c11fec29154e6fdf3a4e96
Saurabh Kumar <saurabh.k0402@gmail.com>
2021-04-20 18:59:33 +0530

    Add more test cases

---
+++ src/more-helpers.test.js
@@ -0,0 +1,156 @@
+const { sum, mul, subLeft } = require('./helpers');
+
+test('Basic two number sum', () => {
+  expect(sum(1, 2)).toBe(3);
+  expect(sum(42, 58)).toBe(100);
+  expect(sum(56, 59)).toBe(115);
+});
+
+test('X numbers sum', () => {
+  expect(sum(1, 2, 3, 4)).toBe(10);
+  expect(sum(50, 60, 70, 80)).toBe(260);
+  expect(sum(11, 22, 33)).toBe(66);
+});
+
+test('Basic two number product', () => {
+  expect(mul(1, 2)).toBe(2);
+  expect(mul(42, 58)).toBe(42 * 58);
+  expect(mul(56, 59)).toBe(56 * 59);
+});
+
+test('X Numbers product', () => {
+  expect(mul(1, 2)).toBe(2);
+  expect(mul(1, 2, 3)).toBe(1 * 2 * 3);
+  expect(mul(1, 2, 3, 4, 5, 6)).toBe(1 * 2 * 3 * 4 * 5 * 6);
+});
+
+test('Basic two number different', () => {
+  expect(subLeft(5, 3)).toBe(2);
+  expect(subLeft(100, 39)).toBe(61);
+  expect(subLeft(50, 35)).toBe(15);
+  expect(subLeft()).toBe(0);
+});
+
+test('Basic two number sum', () => {
+  expect(sum(1, 2)).toBe(3);
+  expect(sum(42, 58)).toBe(100);
+  expect(sum(56, 59)).toBe(115);
+});
+
+test('X numbers sum', () => {
+  expect(sum(1, 2, 3, 4)).toBe(10);
+  expect(sum(50, 60, 70, 80)).toBe(260);
+  expect(sum(11, 22, 33)).toBe(66);
+});
+
+test('Basic two number product', () => {
+  expect(mul(1, 2)).toBe(2);
+  expect(mul(42, 58)).toBe(42 * 58);
+  expect(mul(56, 59)).toBe(56 * 59);
+});
+
+test('X Numbers product', () => {
+  expect(mul(1, 2)).toBe(2);
+  expect(mul(1, 2, 3)).toBe(1 * 2 * 3);
+  expect(mul(1, 2, 3, 4, 5, 6)).toBe(1 * 2 * 3 * 4 * 5 * 6);
+});
+
+test('Basic two number different', () => {
+  expect(subLeft(5, 3)).toBe(2);
+  expect(subLeft(100, 39)).toBe(61);
+  expect(subLeft(50, 35)).toBe(15);
+  expect(subLeft()).toBe(0);
+});
+
+test('Basic two number sum', () => {
+  expect(sum(1, 2)).toBe(3);
+  expect(sum(42, 58)).toBe(100);
+  expect(sum(56, 59)).toBe(115);
+});
+
+test('X numbers sum', () => {
+  expect(sum(1, 2, 3, 4)).toBe(10);
+  expect(sum(50, 60, 70, 80)).toBe(260);
+  expect(sum(11, 22, 33)).toBe(66);
+});
+
+test('Basic two number product', () => {
+  expect(mul(1, 2)).toBe(2);
+  expect(mul(42, 58)).toBe(42 * 58);
+  expect(mul(56, 59)).toBe(56 * 59);
+});
+
+test('X Numbers product', () => {
+  expect(mul(1, 2)).toBe(2);
+  expect(mul(1, 2, 3)).toBe(1 * 2 * 3);
+  expect(mul(1, 2, 3, 4, 5, 6)).toBe(1 * 2 * 3 * 4 * 5 * 6);
+});
+
+test('Basic two number different', () => {
+  expect(subLeft(5, 3)).toBe(2);
+  expect(subLeft(100, 39)).toBe(61);
+  expect(subLeft(50, 35)).toBe(15);
+  expect(subLeft()).toBe(0);
+});
+
+test('Basic two number sum', () => {
+  expect(sum(1, 2)).toBe(3);
+  expect(sum(42, 58)).toBe(100);
+  expect(sum(56, 59)).toBe(115);
+});
+
+test('X numbers sum', () => {
+  expect(sum(1, 2, 3, 4)).toBe(10);
+  expect(sum(50, 60, 70, 80)).toBe(260);
+  expect(sum(11, 22, 33)).toBe(66);
+});
+
+test('Basic two number product', () => {
+  expect(mul(1, 2)).toBe(2);
+  expect(mul(42, 58)).toBe(42 * 58);
+  expect(mul(56, 59)).toBe(56 * 59);
+});
+
+test('X Numbers product', () => {
+  expect(mul(1, 2)).toBe(2);
+  expect(mul(1, 2, 3)).toBe(1 * 2 * 3);
+  expect(mul(1, 2, 3, 4, 5, 6)).toBe(1 * 2 * 3 * 4 * 5 * 6);
+});
+
+test('Basic two number different', () => {
+  expect(subLeft(5, 3)).toBe(2);
+  expect(subLeft(100, 39)).toBe(61);
+  expect(subLeft(50, 35)).toBe(15);
+  expect(subLeft()).toBe(0);
+});
+
+test('Basic two number sum', () => {
+  expect(sum(1, 2)).toBe(3);
+  expect(sum(42, 58)).toBe(100);
+  expect(sum(56, 59)).toBe(115);
+});
+
+test('X numbers sum', () => {
+  expect(sum(1, 2, 3, 4)).toBe(10);
+  expect(sum(50, 60, 70, 80)).toBe(260);
+  expect(sum(11, 22, 33)).toBe(66);
+});
+
+test('Basic two number product', () => {
+  expect(mul(1, 2)).toBe(2);
+  expect(mul(42, 58)).toBe(42 * 58);
+  expect(mul(56, 59)).toBe(56 * 59);
+});
+
+test('X Numbers product', () => {
+  expect(mul(1, 2)).toBe(2);
+  expect(mul(1, 2, 3)).toBe(1 * 2 * 3);
+  expect(mul(1, 2, 3, 4, 5, 6)).toBe(1 * 2 * 3 * 4 * 5 * 6);
+});
+
+test('Basic two number different', () => {
+  expect(subLeft(5, 3)).toBe(2);
+  expect(subLeft(100, 39)).toBe(61);
+  expect(subLeft(50, 35)).toBe(15);
+  expect(subLeft()).toBe(0);
+});
